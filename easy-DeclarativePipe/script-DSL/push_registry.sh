bcd28c5eba9c4e9cd88a2474a224304ff8a30eae
Oleh Avramenko <ext-oleh.avramenko@here.com>
2020-09-20 18:45:10 +0300

    delete folder

--- easy-DeclarativePipe/script-DSL/push_registry.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-GIT_TAG=`git tag --points-at HEAD`
-# CHECK TAG FOR IMAGE
-[ -z "${GIT_TAG}" ] && TAG=${BUILD_NUMBER} || TAG=${GIT_TAG}_${BUILD_NUMBER}
-aws ecr get-login-password --region=${REGION} | docker login --username AWS --password-stdin ${REGISTRY}
-docker tag petclinic-app:${BUILD_NUMBER} ${REGISTRY}:${TAG}
-docker push ${REGISTRY}:$TAG
-docker tag petclinic-app:${BUILD_NUMBER} ${REGISTRY}:latest
-docker push ${REGISTRY}:latest
